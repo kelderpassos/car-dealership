8eaff27c9addc577055edff54c055df9730a2061
Kelder Passos <kelder.passos@gmail.com>
2022-10-21 17:39:29 -0300

    Fix: renaming files

---
+++ src/models/CarsModel.ts
@@ -0,0 +1,21 @@
+import { model as mongooseCreateModel, Schema } from 'mongoose';
+import MongoModel from './MongoModel';
+import { ICar } from '../interfaces/ICar';
+
+const carsMongooseSchema = new Schema<ICar>({
+  model: String,
+  year: Number,
+  color: String,
+  status: Boolean,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+});
+
+class CarsModel extends MongoModel<ICar> {
+  constructor(model = mongooseCreateModel('Cars', carsMongooseSchema)) {
+    super(model);
+  }
+}
+
+export default CarsModel;
