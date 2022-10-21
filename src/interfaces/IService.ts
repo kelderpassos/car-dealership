0e630ea5919e1ae356f324cb70d1bb1dcf96dcbf
Kelder Passos <kelder.passos@gmail.com>
2022-10-21 17:34:09 -0300

    Feat: creating interface IService that sets the attributes service classes must have

---
+++ src/interfaces/IService.ts
@@ -0,0 +1,9 @@
+interface IService<T> {
+  create(obj: unknown): Promise<T>,
+  read(): Promise<T[]>,
+  readOne(_id: string): Promise<T | null>,
+  delete(_id: string): Promise<T | null>,
+  update(_id: string, obj: unknown): Promise<T | null>,
+}
+
+export default IService;
